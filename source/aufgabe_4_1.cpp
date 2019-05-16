6243f94656c3add4a8c0e612c583a60966c675c0
Jonas Roquette <roquettejonas@gmail.com>
2019-05-16 15:13:40 +0200

    finished print() method, rest incomplete

--- source/aufgabe_4_1.cpp
+++ source/aufgabe_4_1.cpp
@@ -1,5 +1,32 @@
 //
 // Created by Jonas Roquette on 2019-05-15.
 //
+#include "circle.hpp"
+#include "vec2.hpp"
+#include <iostream>
 
-#include "aufgabe_4_1.h"
+int main()
+{
+    //Creates an object of Vec2 and RGB
+    Vec2 center{400.f, 400.f};
+    RGB color;
+
+    //Creates multiple circles with different values and names
+    Circle c_1(center, 20.f, color, 1.f, "Yeesus");
+    Circle c_2(center, 400.f, color, 5.f, "Yeetus");
+
+    //Prints the circle values to console
+    std::cout << c_1 << std::endl;
+    std::cout << c_2 << std::endl;
+
+    std::string name;
+    std::cout << "Geben Sie einen Namen ein: " << std::endl;
+    std::cin >> name;
+
+    std::set<std::string> circleList;
+
+    const bool is_in = circleList.find(std::string) != container.end();
+
+    return 0;
+}
+
