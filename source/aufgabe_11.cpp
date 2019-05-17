8ce9ca6dba5c000eda329d16e2810f52f41b8472
Jonas Roquette <roquettejonas@gmail.com>
2019-05-17 14:19:52 +0200

    finished aufgabe_11

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -2,15 +2,38 @@
 // Created by Jonas Roquette on 2019-05-16.
 //
 
-# define CATCH_CONFIG_RUNNER
-# include <catch.hpp>
-# include <cmath>
-# include <algorithm>
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+
+bool remover (int i)
+{
+    return i % 3;
+}
+
+bool is_multiple_of_3 (int i)
+{
+    if (i % 3 == 0)
+    {
+        return true;
+    }
+}
 
 TEST_CASE("filter alle vielfache von drei", "[erase]")
 {
-// ihre Loesung :
-// ...
+    std::vector<unsigned int> v;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        int num = std::rand() % 100 + 1;
+        v.push_back(num);
+    }
+
+    v.erase(std::remove_if(v.begin(), v.end(), remover), v.end());
 
 REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3));
 }
