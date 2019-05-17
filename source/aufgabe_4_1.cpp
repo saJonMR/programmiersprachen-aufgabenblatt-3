931ee77c52eb383186bce9c094f6c90f371db5ab
Jonas Roquette <roquettejonas@gmail.com>
2019-05-17 08:15:44 +0200

    Added insert method and finished assignment

--- source/aufgabe_4_1.cpp
+++ source/aufgabe_4_1.cpp
@@ -3,7 +3,29 @@
 //
 #include "circle.hpp"
 #include "vec2.hpp"
+#include "color.hpp"
 #include <iostream>
+#include <set>
+
+
+//Catches duplicated names when inserting an object of circle into set
+bool insert(std::set<Circle> &setlist, Circle const & circ)
+{
+    bool x = true;
+    for (auto &l : setlist)
+    {
+        if (circ.getName() == l.getName())
+        {
+            x = false;
+        }
+    }
+    if (x)
+    {
+        setlist.insert(circ);
+    } else {
+        std::cout << "Keine Kreise mit dem gleichen Namen einfügen." << std::endl;
+    }
+}
 
 int main()
 {
@@ -14,18 +36,39 @@ int main()
     //Creates multiple circles with different values and names
     Circle c_1(center, 20.f, color, 1.f, "Yeesus");
     Circle c_2(center, 400.f, color, 5.f, "Yeetus");
+    Circle c_3(center, 120.f, color, 2.f, "Jesus");
 
     //Prints the circle values to console
     std::cout << c_1 << std::endl;
     std::cout << c_2 << std::endl;
 
+    //Creates a set for the circles
+    std::set<Circle> circleList;
+
+    //Inserts circles into list
+    insert(circleList, c_1);
+    insert(circleList, c_2);
+    insert(circleList, c_3);
+    insert(circleList, c_1);
+
+    //Asks the user to input a name
     std::string name;
     std::cout << "Geben Sie einen Namen ein: " << std::endl;
     std::cin >> name;
 
-    std::set<std::string> circleList;
-
-    const bool is_in = circleList.find(std::string) != container.end();
+    //Checks whether the name is valid
+    if (name == "")
+    {
+        std::cout << "Der Name ist ungültig, geben Sie erneut einen Namen ein." << std::endl;
+    } else {
+        for (auto& i : circleList)
+        {
+            if (i.getName() == name)
+            {
+                std::cout << i << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
